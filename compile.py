82ff7c338895759779f8df6e5091dc7a145e9c35
Arielle B Cruz <arielle.cruz@gmail.com>
2015-03-08 19:10:07 +0800

    Template now takes some custom strings into account.

--- compile.py
+++ compile.py
@@ -4,8 +4,27 @@
     definitions, SVG rune drawings, and other stuff contained in the HTML
     assets directory.
 """
+import os
 import tempita
+from page_settings import strings
 
 
-if __name__ == "__main__":
-    print "Working on it..."
+context = dict()
+
+# We'll just include our page settings in the context.
+for k, v in strings.items():
+    context[k] = v
+
+# Include the script and the styles.
+for x in ["styles.css", "script.js"]:
+    with open(os.path.join("html-assets", x)) as f:
+        context[x.split(".")[0]] = f.read().decode("utf-8")
+
+
+
+# Do the template substitutions.
+with open("html-assets/main-template.html") as t:
+   page = tempita.sub(t.read().decode("utf-8"), **context)
+
+# Write to file.
+print page
