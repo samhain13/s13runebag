496191b72d12eec9402da4b6c1802d6d326b482f
Arielle B Cruz <arielle.cruz@gmail.com>
2015-03-08 18:17:10 +0800

    Added compile.py and main-template.html

---
+++ compile.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+"""
+    This script generates the static HTML page that contains the rune
+    definitions, SVG rune drawings, and other stuff contained in the HTML
+    assets directory.
+"""
+
+
+
+if __name__ == "__main__":
+    print "Working on it..."
